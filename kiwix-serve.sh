afad4fc0e6f671af33fc1990ac4977c12511bbef
darkenvy <darkenvy@protonmail.com>
2020-12-28 11:44:49 -0800

    removed dockerfile in favor of official file. Updated script

--- kiwix-serve.sh
+++ kiwix-serve.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 CMD=$@
 
 # Check if Root. If not, quit.
@@ -11,14 +12,26 @@ fi
 if [ -z "$1" ]
 then
   echo "No zim specified so kiwix-serve will serve the most recent on :8009"
-  docker pull darkenvy/kiwix-serve
-  docker run -v $(pwd)/mount:/mount -p 8009:8009 darkenvy/kiwix-serve ./kiwix-serve --port=8009 /mount/`ls -t mount | head -1`
+  docker pull kiwix/kiwix-serve
+  sudo docker run \
+    -it \
+    -p 8080:80 \
+    -v $(pwd)/data:/data \
+    kiwix/kiwix-serve \
+    /data/`ls -t data | head -1`
+
 elif [ $1 = 'stop' ]
 then
   echo "Stopping. May take up to 30 seconds"
   docker stop `sudo docker ps -lq --filter="publish=8009"`
 else
   echo "Serving $1 on :8009"
-  docker pull darkenvy/kiwix-serve
-  docker run -v $(pwd)/mount:/mount -p 8009:8009 darkenvy/kiwix-serve ./kiwix-serve --port=8009 /$CMD
+  docker pull kiwix/kiwix-serve
+  sudo docker run \
+    -it \
+    -p 8080:80 \
+    -v $(pwd)/data:/data \
+    kiwix/kiwix-serve \
+    /$CMD
+
 fi
